15beb1744d2d565c9d2b1c85c03b5ba35e4569e2
Necroware <necroware@andjelo.de>
2021-12-21 17:01:32 +0100

    Improved interrupt timing behaviour.
    Removed the Serial.write() call from the interrupt routine. Added a
    FIFO buffer to accumulate the data and moved the Serial.write() into
    the main loop. Now the interrupt should come back almost instantly.

---
+++ Fifo.h
@@ -0,0 +1,59 @@
+#pragma once
+
+/// Simple FIFO implementation.
+///
+/// @param[in] T is the base type of the elements
+/// @param[in] S is the capacity of the buffer
+///
+/// The buffer can be configured to hold up to 255 elements.
+/// If this size will be not enough, the implementation of
+/// class has to be witched to int size, however this would
+/// mean performance penalty on the 8-bit MCU.
+template <typename T, byte S>
+class Fifo {
+  public:
+
+    /// Checks if the buffer is empty.
+    bool empty() const {
+      return m_size == 0;
+    }
+
+    /// Checks if the buffer is full.
+    bool full() const {
+      return m_size >= S;
+    }
+
+    /// Adds an element to the end of the buffer.
+    ///
+    /// @param[in] element is the element to add
+    /// @return false if the buffer is already full
+    bool push(const T& element) {
+      if (full()) {
+        return false;
+      }
+      m_buffer[(m_first + m_size) % S] = element;
+      m_size++;
+      return true;
+    }
+
+    /// Removes an element from the head of the buffer.
+    /// @return The first element, if available
+    T pop() {
+      // If this function was called on an empty buffer, it's the
+      // fault of the user. Normal implementation would throw
+      // an exception, but we don't do such things here, so let's
+      // simply return something;
+      if (empty()) {
+        return T{};
+      }
+
+      const auto result = m_buffer[m_first++];
+      m_size--;
+      return result;
+    }
+
+  private:
+    T m_buffer[S];
+    byte m_first{0};
+    byte m_size{0};
+};
